db85446a26f198d5193c5c1f1af0b718ecf0801a
EriSta98 <erik.stavenius@gmail.com>
2026-06-03 00:29:58 +0200

    Start of project and added dependency for sqlite-jdbc and hibernate

---
+++ src/test/java/se/jensen/erik/cloudstoreproject/CloudStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensen.erik.cloudstoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStoreProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
