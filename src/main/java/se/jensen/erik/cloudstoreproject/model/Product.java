99de346dccf2b97cd122b59fc9918d62d7892895
EriSta98 <erik.stavenius@gmail.com>
2026-06-04 02:56:57 +0200

    Had issues with lombok getters and setters, removed lombok did it the old way

--- src/main/java/se/jensen/erik/cloudstoreproject/model/Product.java
+++ src/main/java/se/jensen/erik/cloudstoreproject/model/Product.java
@@ -6,8 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 
-@Getter
-@Setter
+
 @Entity
 public class Product {
 
@@ -22,5 +21,51 @@ public class Product {
     public Product() {
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
