3e4e76bdd9d564588a0fb3a70933665a57cf800f
EriSta98 <erik.stavenius@gmail.com>
2026-06-05 01:43:56 +0200

    small insignificant changes

--- src/main/java/se/jensen/erik/cloudstoreproject/controller/ProductController.java
+++ src/main/java/se/jensen/erik/cloudstoreproject/controller/ProductController.java
@@ -1,7 +1,6 @@
 package se.jensen.erik.cloudstoreproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.jensen.erik.cloudstoreproject.model.Product;
